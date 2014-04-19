3249960cf1c414a423a62d1134d9266e1887a543
Andrzej Kowalczuk <tieorange@gmail.com>
2014-04-19 17:21:17 +0200

    get small contact photo

--- app/src/main/java/com/tieorange/graycardinal/app/MainActivity.java
+++ app/src/main/java/com/tieorange/graycardinal/app/MainActivity.java
@@ -1,35 +1,109 @@
 package com.tieorange.graycardinal.app;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final int PICK_CONTACT = 1;
+    private final ArrayList<String> mMStringArray1 = new ArrayList<String>();
+    private ListView mUiMyListView;
+    private ImageView mUiContactPhoto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mUiMyListView = (ListView) findViewById(R.id.main_contacts_list);
+        mUiContactPhoto = (ImageView) findViewById(R.id.main_contact_photo);
+
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent, PICK_CONTACT);
 
 
-        ListView myListView = (ListView) findViewById(R.id.main_contacts_list);
-        ArrayList<String> myStringArray1 = new ArrayList<String>();
-        myStringArray1.add("something");
+
+
+        mMStringArray1.add("something");
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,
-                myStringArray1);
-        myListView.setAdapter(adapter);
+                mMStringArray1);
+        mUiMyListView.setAdapter(adapter);
 
+        mMStringArray1.add("something2");
+
+    }
+
+    @Override
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+        switch (reqCode) {
+            case (PICK_CONTACT):
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri contactData = data.getData();
+                    Cursor c = getContentResolver().query(contactData, null, null, null, null);
+
+                    if (c.moveToFirst()) {
+                        String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                        long id = Long.parseLong(c.getString(c.getColumnIndex(ContactsContract.Contacts._ID)));
+                        mUiContactPhoto.setImageBitmap(openPhoto(id));
+                        mMStringArray1.add(name);
+                        // TODO Whatever you want to do with the selected contact name.
+                    }
+                }
+                break;
+        }
+    }
+    public static Bitmap loadContactPhoto(ContentResolver cr, long  id) {
+        Uri uri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, id);
+        InputStream input = ContactsContract.Contacts.openContactPhotoInputStream(cr, uri);
+        if (input == null) {
+            return null;
+        }
+        return BitmapFactory.decodeStream(input);
+    }
+    public Bitmap openPhoto(long contactId) {
+        Uri contactUri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, contactId);
+        Uri photoUri = Uri.withAppendedPath(contactUri, ContactsContract.Contacts.Photo.CONTENT_DIRECTORY);
+        Cursor cursor = getContentResolver().query(photoUri,
+                new String[] {ContactsContract.Contacts.Photo.PHOTO}, null, null, null);
+        if (cursor == null) {
+            return null;
+        }
+        try {
+            if (cursor.moveToFirst()) {
+                byte[] data = cursor.getBlob(0);
+                if (data != null) {
+                    return BitmapFactory.decodeStream(new ByteArrayInputStream(data));
+                }
+            }
+        } finally {
+            cursor.close();
+        }
+        return null;
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
